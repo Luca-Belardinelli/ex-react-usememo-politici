6c45c4d815f963772bd929da586dddc3c329700d
Luca Belardinelli <belardinelliluca@gmail.com>
2025-05-16 09:30:35 +0200

    Implement politician list display and fetch data from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-
+import { useState, useEffect } from "react"
 
 function App() {
 
+  const [politicians, setPoliticians] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/politicians')
+      .then(Response => Response.json())
+      .then(data => setPoliticians(data))
+      .catch(error => console.error(error))
+  }, []);
+
   return (
-    <>
-      <h1>Use_Memo</h1>
-    </>
+    <div>
+      <h1>Lista Politici</h1>
+      <div className="container">
+        {politicians.map(politician => (
+          <div key={politician.id} className="card">
+            <img src={politician.image} alt={politician.name} />
+            <h2>{politician.name}</h2>
+            <h4>{politician.position}</h4>
+            <p>{politician.biography}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
