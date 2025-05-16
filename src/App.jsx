29514bde52ee0c1a18b47b43c2fa13cb78938523
Luca Belardinelli <belardinelliluca@gmail.com>
2025-05-16 10:00:51 +0200

    Add search functionality to filter politicians by name or biography

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useMemo } from "react"
 
 function App() {
 
   const [politicians, setPoliticians] = useState([]);
+
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     fetch('http://localhost:3333/politicians')
@@ -11,11 +13,25 @@ function App() {
       .catch(error => console.error(error))
   }, []);
 
+  const filteredPoliticians = useMemo(() => {
+    return politicians.filter(politician => {
+      const name = politician.name.toLowerCase().includes(search.toLowerCase());
+      const bio = politician.biography.toLowerCase().includes(search.toLowerCase());
+      return name || bio
+    })
+  }, [politicians, search])
+
   return (
     <div>
       <h1>Lista Politici</h1>
+      <input
+        type="text"
+        placeholder='Cerca per nome o biografia'
+        value={search}
+        onChange={e => setSearch(e.target.value)}
+      />
       <div className="container">
-        {politicians.map(politician => (
+        {filteredPoliticians.map(politician => (
           <div key={politician.id} className="card">
             <img src={politician.image} alt={politician.name} />
             <h2>{politician.name}</h2>
